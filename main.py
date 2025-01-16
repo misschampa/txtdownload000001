d207e56be62543326727daaa92a317785ef1e623
krishan041 <krishandara04@gmail.com>
2025-01-16 19:30:46 +0530

    Update and rename main (3).py to main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ import os
 bot = Client(
     "bot",
     bot_token=os.environ.get("BOT_TOKEN"),
-    api_id=int(os.environ.get("API_ID")),
+    api_id=os.environ.get("API_ID"),
     api_hash=os.environ.get("API_HASH")
 )
 
